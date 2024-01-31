b605b7bacdb34d120dd4403792be5d6e0515c93b
Jdubstep1357 <jwlyons2015@gmail.com>
2024-01-31 16:20:44 -0600

    Figured out how to display Javascript in other file

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// REFERENCES FILE Component_Logic.html
+
+  // taskApp is function that returns object
+    let taskApp = () => {
+        return { 
+            tasks: [], 
+            newTask: '',
+            
+            // Function inside of method, use this to reference up stream
+            submit() {
+                this.tasks.push({ body: this.newTask, completed: false }); 
+                this.newTask = '';
+            }
+        }
+    }
